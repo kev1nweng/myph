245a4784c458caf996fe20460fc5bc90ca661d1c
kev1nweng <kw@kev1nweng.space>
2024-07-30 15:25:18 +0800

    Bruteforce 防御 & 代码规范

--- app.py
+++ app.py
@@ -16,25 +16,39 @@ except Exception as e:
 app = fl.Flask(__name__)
 CORS(app)
 
-globalDelay = 0
-debugMode = False
 
-if debugMode:
-    globalDelay = 2
-else:
-    globalDelay = 0
+class Debug:
+    enabled = True
+    simulatedDelay = 0
 
-activeSessionToken = None
-tokenUpdateTime = None
-fingerprintMD5 = None
+    def sleep():
+        time.sleep(Debug.simulatedDelay if Debug.enabled else 0)
+
+
+class Runtime:
+    class token:
+        value = None
+        updateTime = None
+        accessBuffer = 0
+
+    class fingerprint:
+        md5 = None
+
+    def antiBruteforce():
+        if Runtime.token.accessBuffer > 5:
+            print("\n[!] **疑似攻击** 已阻塞过于频繁的请求。\n")
+            fl.abort(418)
+
 
 # 读取用户指纹，若没有指纹则进行生成
 while True:
     try:
         with open("./fingerprint", "rb") as file:
             fingerprint = file.read(4096).hex()
-            fingerprintMD5 = hashlib.md5(fingerprint.encode("utf-8")).hexdigest()
-            print(f"\n[+] 已找到指纹。 文件的 md5 是 {fingerprintMD5}\n")
+            Runtime.fingerprint.md5 = hashlib.md5(
+                fingerprint.encode("utf-8")
+            ).hexdigest()
+            print(f"\n[+] 已找到指纹。 文件的 md5 是 {Runtime.fingerprint.md5}\n")
         break
     except Exception as e:
         print(f"\n[-] 未找到指纹。正在生成...\n")
@@ -43,7 +57,7 @@ while True:
 
 
 # 存储密码特征信息
-class pwdSpec:
+class Spec:
     try:
         set = str(config.get("pwd", "set"))
         rule = str(config.get("pwd", "rule"))
@@ -59,53 +73,48 @@ class pwdSpec:
 
 
 def updateConfig():
-    global pwdSpec
+    global Spec
     config.read("config.ini")
     try:
-        pwdSpec.set = str(config.get("pwd", "set"))
-        pwdSpec.rule = str(config.get("pwd", "rule"))
-        pwdSpec.host = str(config.get("pwd", "host"))
-        pwdSpec.prefix = str(config.get("pwd", "prefix"))
-        pwdSpec.hashlength = int(config.get("pwd", "hashlength"))
-        pwdSpec.sg1 = int(config.get("pwd", "seed1segment"))
-        pwdSpec.sg2 = int(config.get("pwd", "seed2segment"))
-        pwdSpec.suffix = str(config.get("pwd", "suffix"))
+        Spec.set = str(config.get("pwd", "set"))
+        Spec.rule = str(config.get("pwd", "rule"))
+        Spec.host = str(config.get("pwd", "host"))
+        Spec.prefix = str(config.get("pwd", "prefix"))
+        Spec.hashlength = int(config.get("pwd", "hashlength"))
+        Spec.sg1 = int(config.get("pwd", "seed1segment"))
+        Spec.sg2 = int(config.get("pwd", "seed2segment"))
+        Spec.suffix = str(config.get("pwd", "suffix"))
     except BaseException:
         print("\n[-] 配置已损坏。程序正在退出...\n")
         sys.exit(0)
 
 
-if pwdSpec.set == "false":
+if Spec.set == "false":
     print("[!] 服务器未正确配置。\n")
 
 
 # 生成10分钟有效期的会话令牌
 def generateToken():
-    global activeSessionToken, tokenUpdateTime
+    global Runtime, Runtime
     sKey = hashlib.md5(str(random.random()).encode()).hexdigest()
-    activeSessionToken = sKey
-    tokenUpdateTime = datetime.datetime.now()
+    Runtime.token.value = sKey
+    Runtime.token.updateTime = datetime.datetime.now()
     return sKey
-
-
-# 模拟延迟
-def debugSleep():
-    time.sleep(globalDelay)
 
 
 # 获取服务器状态的接口
 @app.route("/ping")
 def index():
-    global fingerprintMD5
-    debugSleep()
-    return fl.jsonify({"fingerprint": fingerprintMD5, "setup": pwdSpec.set})
+    global Runtime
+    Debug.sleep()
+    return fl.jsonify({"fingerprint": Runtime.fingerprint.md5, "setup": Spec.set})
 
 
 @app.route("/submitConfig")
 def submitConfig():
-    global config, updateConfig, activeSessionToken
+    global config, updateConfig, Runtime
 
-    if (fl.request.args.get("token") != activeSessionToken) and pwdSpec.set == True:
+    if (fl.request.args.get("token") != Runtime.token.value) and Spec.set == True:
         fl.abort(403)
 
     class pwdConfigParams:
@@ -128,14 +137,17 @@ def submitConfig():
     print("\n[!] 配置文件已更改。\n")
     updateConfig()
     print("[i] 配置热重载完成。\n")
-    debugSleep()
+    Debug.sleep()
     return fl.jsonify({"status": "success"})
 
 
 # 秘符验证接口（基于当前时间的动态密码）
 @app.route("/auth/<string:key>")
 def auth(key):
-    global activeSessionToken, pwdSpec
+    global Runtime
+    Runtime.token.accessBuffer += 1
+    Runtime.antiBruteforce()
+
     now = datetime.datetime.now()
     year = str(now.year % 100)
     year = str(now.year % 100)
@@ -163,7 +175,7 @@ def auth(key):
 
     def getDnComSeq():
         seq = ""
-        for i in pwdSpec.rule.split("-"):
+        for i in Spec.rule.split("-"):
             match i:
                 case "d1":
                     seq += snippets.d1
@@ -192,85 +204,92 @@ def auth(key):
         return seq
 
     dynamicComputedSequence = getDnComSeq()
-    debugSleep()
+    Debug.sleep()
 
     if key == dynamicComputedSequence:
         ntk = generateToken()
-        print(f"\n[+] Access granted for {activeSessionToken}.\n")
+        print(f"\n[+] Access granted for {Runtime.token.value}.\n")
         return {"access": True, "token": ntk}
     else:
         print(
             f"\n[-] 已拒绝访问，因为提供的秘符 {key} 与预期序列 {dynamicComputedSequence} 不匹配。\n"
         )
-        if activeSessionToken is not None:
+        if Runtime.token.value is not None:
             if key != "-logout-":
                 print(
-                    f"[!] 由于出现验证故障，正在使密钥 {activeSessionToken} 失效。 \n"
+                    f"[!] 由于出现验证故障，正在使密钥 {Runtime.token.value} 失效。 \n"
                     # 安全措施，若另一个用户在别处输入了错误的密码则一刀切使所有会话失效，有待商榷？
                 )
             else:
                 print("[!] 收到注销会话请求，正在使会话密钥失效 \n")
-            activeSessionToken = None
+            Runtime.token.value = None
         return {"access": False, "token": None}
 
 
 # 获取密码的接口
 @app.route("/fetchKey")
 def api():
+    global Runtime
+    Runtime.token.accessBuffer += 1
+    Runtime.antiBruteforce()
+
     # activeSessionToken 为实现会话有效期使用的动态 token
     # pwdSpec 为包含了用户自定义密码规则的 class
-    global activeSessionToken, pwdSpec
     token = fl.request.args.get("token")
-    if token != activeSessionToken:
-        fl.abort(403)
+    if token != Runtime.token.value:
+        fl.abort(401)
     inputStr = fl.request.args.get(
         "id"
     ).upper()  # 该变量为用户提供的平台名称的 SHA1 不可逆加密结果
     calcid = f"{inputStr}-{fingerprint}"
     symbols = ["!", "@", "#", "$", "&", "%", "/"]  # 使用符号增加密码复杂度
     hashPart = hashlib.sha256(calcid.encode(encoding="utf-8")).hexdigest()[
-        : pwdSpec.hashlength
+        : Spec.hashlength
     ]
     # 使用哈希序列的切片作为随机种子
-    seeds = [hashPart[: pwdSpec.sg1], hashPart[: pwdSpec.sg2]]
+    seeds = [hashPart[: Spec.sg1], hashPart[: Spec.sg2]]
     symbolPart = ""
     for seed in seeds:
         random.seed(seed)
         symbolPart += random.choice(symbols)
     # 凭借不同部分组成密码并返回
-    pwd = pwdSpec.prefix + hashPart + symbolPart + pwdSpec.suffix
+    pwd = Spec.prefix + hashPart + symbolPart + Spec.suffix
 
-    debugSleep()
+    Debug.sleep()
     return {"id": inputStr, "pwd": pwd}
 
 
 # 获取服务器配置状态的接口
 @app.route("/cfgStatus")
 def getIsConfigured():
-    global pwdSpec
-    if pwdSpec.set == "true":
+    global Spec
+    if Spec.set == "true":
         return fl.jsonify({"configured": True})
     else:
         return fl.jsonify({"configured": False})
 
 
 # 重置会话令牌的函数
-def resetActiveSessionToken():
-    global activeSessionToken, tokenUpdateTime
+def tokenDaemonPayload():
+    global Runtime
     now = datetime.datetime.now()
     expiry = datetime.timedelta(minutes=10)
-    if activeSessionToken is None:
+    if Runtime.token.value is None:
         return
-    if activeSessionToken is not None and now > tokenUpdateTime + expiry:
-        print(f"\n[!] 活动的会话令牌 {activeSessionToken} 已过期。 \n")
-        activeSessionToken = None
-        tokenUpdateTime = None
+    if Runtime.token.value is not None and now > Runtime.token.updateTime + expiry:
+        print(f"\n[!] 活动的会话令牌 {Runtime.token.value} 已过期。 \n")
+        Runtime.token.value = None
+        Runtime.token.updateTime = None
 
 
 # 定期expire会话令牌的线程
 def activeSessionTokenMonitor():
+    global Runtime
     while True:
-        resetActiveSessionToken()
+        tokenDaemonPayload()
+        Runtime.token.accessBuffer = (
+            Runtime.token.accessBuffer - 1 if Runtime.token.accessBuffer > 0 else 0
+        )
         time.sleep(1)
 
 
@@ -280,6 +299,7 @@ sessionTokenMonitor.start()
 
 # ^C处理函数
 def signalHandler(signal, frame):
+    signal, frame  # 防止提示变量未使用
     print("\n[!] 正在退出")
     sys.exit(0)
 
@@ -287,4 +307,4 @@ def signalHandler(signal, frame):
 signal.signal(signal.SIGINT, signalHandler)
 
 if __name__ == "__main__":
-    app.run(host=pwdSpec.host, port=4518, debug=debugMode)
+    app.run(host=Spec.host, port=4518, debug=Debug.enabled)
